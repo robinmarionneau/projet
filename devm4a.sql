4466800c89c8b5de3aab1e144ed9513002343e89
Robin <112472287+robinmarionneau@users.noreply.github.com>
2022-09-15 16:58:02 +0200

    Update devm4a.sql

--- devm4a.sql
+++ devm4a.sql
@@ -1,6 +1,6 @@
 create table play (
         id_movie integer not null, 
         id_actor integer not null);
-
-alter table play add constraint FK9lf0huabanmd36i9ffruceti9 foreign key (id_actor) references stars;
-alter table play add constraint FKn74w97h2tnfs8kq7ockm5kvu3 foreign key (id_movie) references movies;
+alter table movies add constraint fk_moviesstars foreign key (id_director) references stars(id);
+alter table play add constraint fk_playstars foreign key (id_actor) references stars(id);
+alter table play add constraint fk_playmovies foreign key (id_movie) references movies(id);
