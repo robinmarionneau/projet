d95a357cefbc4054916bd6912d3548d789df2994
Robin <112472287+robinmarionneau@users.noreply.github.com>
2022-09-15 16:07:37 +0200

    Update devm4a.sql

--- devm4a.sql
+++ devm4a.sql
@@ -1,11 +1,6 @@
-create table [IF NOT EXISTS] play (
+create table play (
         id_movie integer not null, 
         id_actor integer not null);
-
-create table [IF NOT EXISTS] stars (id integer auto_increment, 
-        birthdate date, 
-        name varchar(100) not null, 
-        primary key (id));
 
 alter table movies add constraint FK5sptslekmtqse3ukuv4s9iyp6 foreign key (id_director) references stars;
 alter table play add constraint FK9lf0huabanmd36i9ffruceti9 foreign key (id_actor) references stars;
